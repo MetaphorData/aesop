09c4bebc31b613c9803c0232e4b043df12f290d1
Tsung-Ju Lii <andy@metaphor.io>
2024-09-23 20:26:51 +0800

    [sc-27924] List governed tags (#8)
    * chore: update ariadne-codegen to latest
    
    * [sc-27924] List governed tags
    
    * add codeowners

--- aesop/app.py
+++ aesop/app.py
@@ -2,18 +2,25 @@ import typer
 import yaml
 from typing_extensions import Annotated
 
-from aesop.commands import info_command, upload_command
+from aesop.commands import info_command, tags_app, upload_command
+from aesop.commands.common.enums.output_format import OutputFormat
 from aesop.config import DEFAULT_CONFIG_PATH, AesopConfig
 
 app = typer.Typer(add_completion=False)
+app.add_typer(tags_app, name="tags")
 
 
 @app.command()
 def info(
     ctx: typer.Context,
+    output: OutputFormat = typer.Option(
+        default=OutputFormat.TABULAR,
+        help="The output format. "
+        f"Supported formats: [{', '.join(f for f in OutputFormat)}]",
+    ),
 ) -> None:
     "Display information about the Metaphor instance."
-    info_command(ctx.obj)
+    info_command(output, ctx.obj)
 
 
 @app.command()
@@ -23,7 +30,7 @@ def upload(
         ...,
         help="Path to the CSV file containing data asset information",
     ),
-):
+) -> None:
     """
     Upload data assets from a CSV file.
     """
@@ -35,8 +42,8 @@ def main(
     ctx: typer.Context,
     config_file: Annotated[
         typer.FileText, typer.Option(help="Path to the configuration file.")
-    ] = DEFAULT_CONFIG_PATH.as_posix(),
-):
+    ] = DEFAULT_CONFIG_PATH.as_posix(),  # type: ignore
+) -> None:
     ctx.obj = AesopConfig.model_validate(yaml.safe_load(config_file))
 
 
--- aesop/commands/__init__.py
+++ aesop/commands/__init__.py
@@ -1,7 +1,9 @@
 from .info.info import info as info_command
+from .tags.tags import app as tags_app
 from .upload.upload import upload as upload_command
 
 __all__ = [
     "upload_command",
     "info_command",
+    "tags_app",
 ]
