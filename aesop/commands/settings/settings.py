4f391c06d2ce69b95989ad3f51ffc98d1330a452
Tsung-Ju Lii <andy@metaphor.io>
2024-09-23 21:28:38 +0800

    address comments

--- aesop/commands/settings/settings.py
+++ aesop/commands/settings/settings.py
@@ -5,6 +5,6 @@ from .non_prod.non_prod import app as non_prod_app
 from .soft_deletion.soft_deletion import app as soft_deletion_app
 
 app = typer.Typer(help="Settings")
-app.add_typer(non_prod_app, name="non_prod")
-app.add_typer(custom_metadata_app, name="custom_metadata")
-app.add_typer(soft_deletion_app, name="soft_deletion")
+app.add_typer(non_prod_app, name="non-prod")
+app.add_typer(custom_metadata_app, name="custom-metadata")
+app.add_typer(soft_deletion_app, name="soft-deletion")
