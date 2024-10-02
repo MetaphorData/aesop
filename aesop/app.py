a20bc2986b151a582c9cdcafd95a5ee36e888956
Tsung-Ju Lii <andy@metaphor.io>
2024-10-02 21:52:43 +0800

    [sc-28993] `aesop tags` command (#21)
    * [sc-28993] `aesop tags add` take 2
    
    * [sc-28993] Remove, assign, and unassign tags (#22)
    
    * [sc-28993] `aesop tags remove`
    
    * add assign , unassign, and give more help when no input is provided
    
    * address comments
    
    * refactor
    
    * refactor paginator, add get aspect command
    
    * add test

--- aesop/app.py
+++ aesop/app.py
@@ -5,14 +5,21 @@ import yaml
 from rich import print
 from typing_extensions import Annotated
 
-from aesop.commands import info_command, settings_app, tags_app, upload_command
+from aesop.commands import (
+    datasets_app,
+    info_command,
+    settings_app,
+    tags_app,
+    upload_command,
+)
 from aesop.commands.common.enums.output_format import OutputFormat
 from aesop.commands.common.exception_handler import exception_handler
 from aesop.config import DEFAULT_CONFIG_PATH, AesopConfig
 
-app = typer.Typer(add_completion=False)
+app = typer.Typer(add_completion=False, rich_markup_mode="markdown")
 app.add_typer(tags_app, name="tags")
 app.add_typer(settings_app, name="settings")
+app.add_typer(datasets_app, name="datasets")
 
 
 @app.command()
