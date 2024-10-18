42388ba183f055d9ba84dc6ea74994b1debdcaa8
Tsung-Ju Lii <andy@metaphor.io>
2024-10-18 23:56:46 +0800

    [sc-29289] Import biz glossary to Metaphor data document store (#27)
    * [sc-29289] Import local file to Metaphor data document store
    
    * address comments
    
    * address comments
    
    * address comments

--- aesop/commands/__init__.py
+++ aesop/commands/__init__.py
@@ -1,11 +1,13 @@
 from .aspects import tags_app
+from .documents.documents import app as documents_app
 from .entities import datasets_app
-from .info.info import info as info_command
+from .info import info as info_command
 from .settings.settings import app as settings_app
 from .upload.upload import upload as upload_command
-from .webhooks.webhooks import app as webhooks_app
+from .webhooks import app as webhooks_app
 
 __all__ = [
+    "documents_app",
     "upload_command",
     "info_command",
     "settings_app",
